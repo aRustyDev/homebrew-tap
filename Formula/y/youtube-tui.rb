3a29d1cdeac6bb1d6e7ce6a5f93064c274dd2261
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2025-09-29 15:23:43 -0400

    feat: added external formulas

---
+++ Formula/y/youtube-tui.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class YoutubeTui < Formula
+  desc ""
+  homepage ""
+  license ""
+  head "git@github.com:Siriusmart/youtube-tui.git"
+
+  depends_on "rust" => :build
+  depends_on "yt-dlp" "youtube-downloader"
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test aws-sso`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "true"
+  end
+end
